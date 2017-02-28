d23a872881271c055fcf85d4ac87a4f5229939b2
Renat Zubairov <renat.zubairov@gmail.com>
2017-02-28 12:22:19 +0100

    Now generating proper schemas

--- spec/metadata.spec.js
+++ spec/metadata.spec.js
@@ -5,7 +5,9 @@ describe('Given the mappings', () => {
     const schemas = require('../lib/schemas.js');
 
     it('should list properties', () => {
-        const result = schemas('foo');
+        const result = schemas('miscellaneous');
         expect(Object.keys(result).length).to.equal(25);
+        expect(Object.keys(result.descriptionData.properties).length).to.equal(47);
+        expect(Object.keys(result.productData.properties.miscellaneous.properties).length).to.equal(11);
     });
 });
